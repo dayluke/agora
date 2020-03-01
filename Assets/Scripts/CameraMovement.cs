009529e77ed2c714032264a4079718373957cd4d
Luke Day <ld227707@falmouth.ac.uk>
2020-03-01 03:24:58 +0000

    Git issues

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 10f;
+    [SerializeField] private float yAxisClamp = 80f;
+    private GameObject player;
+    private float cameraRot = 0;
+
+    private void Start()
+    {
+        player = transform.parent.gameObject;
+    }
+    
+    private void Update()
+    {
+        Vector3 mouseInput = GetMouseInput();
+
+        // Y axis rotation (Player)
+        player.transform.Rotate(Vector3.up * mouseInput.x * mouseSensitivity);
+
+        // X axis rotation (Camera)
+        cameraRot += -mouseInput.y * mouseSensitivity;
+        cameraRot = Mathf.Clamp(cameraRot, -yAxisClamp, yAxisClamp);
+        transform.localEulerAngles = new Vector3(cameraRot, transform.localEulerAngles.y, 0);
+    }
+
+    private Vector3 GetMouseInput()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        return new Vector3(x, y, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float yForce = 1f;
+    [SerializeField] private float resistanceDrag = 10f;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 movementInput = GetMovement();
+        rb.velocity = transform.TransformDirection(movementInput);
+    }
+
+    private Vector3 GetMovement()
+    {
+        float x, y, z;
+
+        x = Input.GetAxis("Horizontal");
+        y = HandleYMovement();
+        z = Input.GetAxis("Vertical");
+
+        return new Vector3(x * speed, y, z * speed);
+    }
+
+    private float HandleYMovement()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            return yForce;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            return -yForce;
+        }
+        else
+        {
+            return Mathf.Lerp(rb.velocity.y, 0, Time.deltaTime * resistanceDrag);
+        }
+    }
+}
